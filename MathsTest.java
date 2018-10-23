0574b0a134a80e78b1d2bbc4e10b6dc82bfa4a43
anirudhlokapur <anirudhlokapur@gmail.com>
2018-10-23 18:25:23 +0530

    Add files via upload

---
+++ MathsTest.java
@@ -0,0 +1,52 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MathsTest {
+	
+	Maths m;
+
+	@Before
+	public void setUp() throws Exception {
+		
+		m=new Maths();
+	}
+
+	
+	/*
+	 * 1. when the number is zero system should return one
+	 * 2. when the number is one system should return one
+	 * 3. when the number is positive system should return expected output
+	 *    e.g. 4 - 24
+	 * 4. when the number is negative system should throw exception
+	
+	 * 
+	 */
+	
+	@Test
+	public void whenTheNumberIsZeroSystemShouldReturnOne()
+	{
+		assertEquals(1, m.fact(0));
+	}
+	
+	@Test(expected=java.lang.IllegalArgumentException.class)
+	public void whenTheNumberIsNegativeSystemShouldThrowException()
+	{
+		m.fact(-4);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
